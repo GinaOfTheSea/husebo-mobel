c96071640dee3b65d6ee1f8f6281d5523f1c3102
Gina <gina.husebo@outlook.com>
2026-02-25 14:06:19 +0100

    First Tommy Commit

---
+++ src/portfolio/båt-i-vinden.md
@@ -0,0 +1,4 @@
+---
+title: Båt i vinden
+photo: /static/img/6ee9c5c2-3f46-4d55-82b2-8d5bb425d085.jpeg
+---
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: ["./**/*.html"],
+  content: [
+    // 1. Path to your main HTML file(s) in the root
+    "./index.html", 
+    // 2. Path to any HTML/JS files in your source folder
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
   theme: {
     container: {
       center: true,
