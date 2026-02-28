11edf07dcc23a049f5a8d16acd9050058f44ef83
Gina <gina.husebo@outlook.com>
2026-02-28 01:52:48 +0100

    Max width edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
     },
     extend: {
       colors: {},
+      fontFamily: {
+        // This sets Inter as the primary sans-serif font
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+      },
     },
   },
   variants: {},
